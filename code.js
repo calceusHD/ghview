6a7d86e0a5552afe9cb982adb91188e66f040455
calceushd <calceushd@gmail.com>
2019-01-07 20:47:05 +0100

    complete the tree view

--- code.js
+++ code.js
@@ -312,14 +312,14 @@ ghw = (function() {
                 var len = type.nbr_scalars;
                 var stride = self.get_nbr_elements(type.base.el);
                 for (var i = 0; i < len; i += stride) {
-                    self.read_signal(handler, data, sigs, type.base.el, i);
+                    self.read_signal(handler, data, sigs, type.base.el, offset + i);
                 }
                 break;
             case rtik.type_record:
                 var nbr_fields = type.els.length;
                 var off = 0;
                 for (var i = 0; i < nbr_fields; ++i) {
-                    self.read_signal(handler, data, sigs, type.els[i].type, off);
+                    self.read_signal(handler, data, sigs, type.els[i].type, offset + off);
                     off += self.get_nbr_elements(type.els[i].type);
                 }
                 break;
@@ -512,7 +512,8 @@ ghw = (function() {
         rv.hie = {};
         rv.sigs = [];
         rv.nbr_sigs = nbr_basic_sigs + 1;
-        rv.hie.name = "";
+        rv.hie.name = "top";
+        rv.hie.kind = 1;
         var stack = [rv.hie];
         var parent = rv.hie;
         while (true) {
@@ -713,7 +714,7 @@ ghw = (function() {
             this.name = name;
             this.buffer = "";
             this.buffer_os = 0;
-            this.block_size = 4096*4;
+            this.block_size = 1024*32;
             this.offset = 0;
         }
 
@@ -747,10 +748,19 @@ ghw = (function() {
         }
     };
 
-    self.print_hie = function(hie, prefix) {
-        rv = "<li><span>" + prefix + hie.name
+    var reverse_hie_kind = Object.keys(hie_kind).reduce(function(obj,key){
+        obj[ hie_kind[key] ] = key;
+        return obj;
+     },{});
+/*
+    self.print_array_subtype = function(type, sigs, name, stack, offset, ico) {
+        
+    };
+*/
+    self.print_hie = function(hie, stack) {
+        var rv = "<li><span draggable='true' ondragstart='drag(event)' class='hie_elem' data-location='" + stack + "'>" + "<img onclick='collapse(this)' src='art/ico-" + reverse_hie_kind[hie.kind] + ".svg'></img>" + hie.name;
         if ("type" in hie) {
-            if (hie.type.kind == 35) {
+            if (hie.type.name == "<anon>") {
                 rv += " : " + hie.type.base.name;
             } else {
                 rv += " : " + hie.type.name;
@@ -763,54 +773,223 @@ ghw = (function() {
                 rv += ": #" + hie.sigs[0];
             }
         }
-        rv += "</span>" + "<ul>";
-        if ("children" in hie) {
-            for (child of hie.children) {
-                rv += self.print_hie(child, prefix + " ");
-            }
+        rv += "</span>";
+        if (("type" in hie) && hie.type.kind == rtik.subtype_array) {
+            rv += "<ul class='collapsed'>";
+            rv += self.print_array_subtype(hie.type, hie.sigs, hie.name, stack, 0, reverse_hie_kind[hie.kind]);
+            rv += "</ul>";
         }
-        return rv + "</li></ul>";
+        if ("children" in hie) {
+            rv += "<ul class='collapsed'>";
+            for ([p, child] of Object.entries(hie.children)) {
+                rv += self.print_hie(child, stack + p + " ");
+            }
+            rv += "</ul>";
+        }
+        return rv + "</li>";
     };
 
     self.convert_values = function(vals, handler) {
         var rv = [];
-        for (var i = 0; i < handler.nbr_sigs; ++i) {
-            var el = {};
-            el.steps = [];
-            for (var val of vals) {
-                if (i in val.sigs) {
-                    var v = {};
-                    v.snap_time = val.snap_time;
-                    v.value = val.sigs[i];
-                    el.steps.push(v);
-                }
+        for (var i = 0; i < handler.sigs.length; ++i) {
+            rv[i] = [];
+        }
+        for (var o of vals) {
+            for (var j of Object.entries(o.sigs)) {
+                rv[j[0]].push({snap_time: o.snap_time, value: j[1]});
             }
-
-            rv.push(el);
         }
         return rv;
+    };
+
+    self.location_root = function(hie, location) {
+        location = location.trim();
+        if (!location || location.length == 0) {
+            return [hie, []];
+        }
+        var hoops = location.split(" ");
+        var rv = hie;
+        var i = 0;
+        for (var hoop of hoops) {
+            if ("children" in rv) {
+                rv = rv.children[hoop];
+                ++i;
+            } else {
+                return [rv, hoops.slice(i)];
+            }
+        }
+        return [rv, []];
+    }
+
+    self.location_offset = function(type, location) {
+        var rv = 0;
+        var root = type;
+        for (loc of location) {
+            if (root.kind == rtik.subtype_array) {
+                root = root.base.el;
+                rv += loc * self.get_nbr_elements(root);
+            } else if (root.kind == rtik.type_record || root.kind == rtik.subtype_record) {
+                for (var i = 0; i < loc; ++i) {
+                    rv += self.get_nbr_elements(root.els[i].type);
+                }
+                root = root.els[loc].type;
+            }
+        }
+        return [rv, root];
+    }
+
+    self.print_child_hie = function(location, children) {
+        var rv = "";
+        rv += "<ul>";
+        for ([p, child] of Object.entries(children)) {
+            rv += "<li><span draggable='true' ondragstart='drag(event)' class='hie_elem' data-location='" + location + " " + p + "'>" + "<img onclick='collapse(this)' src='art/ico-" + reverse_hie_kind[child.kind] + ".svg'></img>" + child.name;
+            if ("type" in child) {
+                if (child.type.name == "<anon>") {
+                    rv += " : " + child.type.base.name;
+                } else {
+                    rv += " : " + child.type.name;
+                }
+            }
+            if ("sigs" in child) {
+                if (child.sigs.length > 1) {
+                    rv += ": #" + child.sigs[0] + "-#" + child.sigs[child.sigs.length-1];
+                } else {
+                    rv += ": #" + child.sigs[0];
+                }
+            }
+            rv += "</span></li>";
+        }
+        rv += "</ul>";
+        return rv;
+    }
+
+    self.print_hie_partial = function(hie, location) {
+        var rv = "";
+        var [root, remainder] = self.location_root(hie, location);
+        if (!root) {
+            return;
+        }
+        if ("children" in root) {
+            rv += self.print_child_hie(location, root.children);
+        } else if ("type" in root && (root.type.kind == rtik.subtype_array || root.type.kind == rtik.type_record || root.type.kind == rtik.subtype_record)) {
+            var [offs, type] = self.location_offset(root.type, remainder);
+            rv += "<ul>";
+            if (type.kind == rtik.subtype_array) {
+                var tot_num = 1;
+                var digits = [];
+                var arr_len = [];
+                var arr_dir = [];
+                for (var [i, rng] of Object.entries(type.rngs)) {
+                    var tmp = Math.abs(rng.left - rng.right) + 1;
+                    tot_num *= tmp;
+                    arr_len[i] = rng.right;
+                    digits[i] = rng.left;
+                    arr_dir[i] = Math.sign(rng.right - rng.left);
+                    
+                }
+                var stride = self.get_nbr_elements(type.base.el);
+                name = "";
+                for (var num = 0; num < tot_num; ++num) {
+                    var cur_name = name + "[" + digits.join(", ") + "]";
+                    rv += "<li><span draggable='true' ondragstart='drag(event)' class='hie_elem' data-location='" + location + " " + num + "'>" + "<img onclick='collapse(this)' src='art/ico-" + reverse_hie_kind[root.kind] + ".svg'></img>" + cur_name;
+                    if (type.base.el.name == "<anon>") {
+                        rv += " : " + type.base.el.base.name;
+                    } else {
+                        rv += " : " + type.base.el.name;
+                    }
+                    
+                    if (stride > 1) {
+                        rv += ": #" + root.sigs[offs + num * stride] + "-#" + root.sigs[offs + (num + 1) * stride - 1];
+                    } else {
+                        rv += ": #" + root.sigs[offs + num * stride];
+                    }
+                    rv += "</span>";
+                    rv += "</li>";    
+                    for (var idx = 0; idx < type.rngs.length; ++idx) {
+                        if (digits[idx] == arr_len[idx]) {
+                            digits[idx] = 0;
+                        } else {
+                            digits[idx] += arr_dir[idx];
+                            break;
+                        }
+                    }
+                }
+            } else if (type.kind == rtik.type_record || type.kind == rtik.subtype_record) {
+                for (var [i, el] of Object.entries(type.els)) {
+                    rv += "<li><span draggable='true' ondragstart='drag(event)' class='hie_elem' data-location='" + location + " " + i + "'>" + "<img onclick='collapse(this)' src='art/ico-" + reverse_hie_kind[root.kind] + ".svg'></img>" + el.name;
+                    if (el.type.name == "<anon>") {
+                        rv += " : " + el.type.base.name;
+                    } else {
+                        rv += " : " + el.type.name;
+                    }
+                    var el_len = self.get_nbr_elements(el.type);
+                    if (el_len > 1) {
+                        rv += ": #" + root.sigs[offs] + "-#" + root.sigs[offs + el_len - 1];
+                    } else {
+                        rv += ": #" + root.sigs[offs];
+                    }
+                    offs += el_len;
+                    rv += "</span></li>";
+                }
+            }
+            rv += "</ul>";
+        }
+        
+        return rv;
+    };
+
+    self.print_hie_root = function(hie) {
+        var rv = "<li><span draggable='true' ondragstart='drag(event)' class='hie_elem' data-location=''>" + "<img onclick='collapse(this)' src='art/ico-" + reverse_hie_kind[hie.kind] + ".svg'></img>" + hie.name;
+        return rv;
+    }
+
+    self.get_signal = function(hie, location) {
+        var rv = "";
+        var [root, remainder] = self.location_root(hie, location);
+        if (!root) {
+            return;
+        }
+        switch (root.kind) {
+            case hie_kind.signal:
+            case hie_kind.port_in:
+            case hie_kind.port_out:
+            case hie_kind.port_inout:
+            case hie_kind.port_buffer:
+            case hie_kind.port_linkage:
+
+            default:
+                rv += "this aint no signal";
+        }
     }
 
     return self;
 })();
 
 onmessage = function(ev) {
-    var file_el = ev.data;
-    var f = new ghw.File(file_el);
-    var handler = {};
-    ghw.open_ghw(f, handler);
-    ghw.read_headers(f, handler);
-    var msg = {};
-    msg.type = "HIE";
-    msg.val = ghw.print_hie(handler.hie, "");
-    postMessage(msg);
-    
-    var values = [];
-    ghw.read_dump(f, handler, values);
-    ghw.read_dump(f, handler, values);
-    var sigs = ghw.convert_values(values, handler);
-    msg.type = "VAL";
-    msg.val = sigs;
-    postMessage(msg);
-    
+    var req = ev.data.type;
+    if (req == "LOAD_FILE") {
+        var file_el = ev.data.val;
+        var f = new ghw.File(file_el);
+        handler = {};
+        ghw.open_ghw(f, handler);
+        ghw.read_headers(f, handler);
+        var msg = {};
+        msg.type = "HIE";
+        msg.val = ghw.print_hie_root(handler.hie);
+        postMessage(msg);
+        
+        var values = [];
+        ghw.read_dump(f, handler, values);
+        ghw.read_dump(f, handler, values);
+        var sigs = ghw.convert_values(values, handler);
+        msg.type = "VAL";
+        msg.val = {sigs :sigs, end_time: values[values.length-1].snap_time};
+        postMessage(msg);
+    } else if (req == "GET_INNER") {
+        var rv = ghw.print_hie_partial(handler.hie, ev.data.val);
+        postMessage({type: "INNER", val: {loc: ev.data.val, html: rv}});
+    } else if (req == "GET_SIGNAL") {
+        var rv = ghw.get_signal(handler.hie, ev.data.val);
+        postMessage({type: "SIGNAL", val: {html: rv}});
+    }
 }
